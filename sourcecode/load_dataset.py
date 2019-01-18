cbc18d53dbb5b39e7a8d686e297448faeb5fab9a
Randy <randybruno424@gmail.com>
2019-01-17 20:29:02 -0500

    update

---
+++ sourcecode/load_dataset.py
@@ -0,0 +1,13 @@
+import sys
+import os
+from netCDF4 import Dataset
+
+
+sys.path.append('../data/WTDL_pisces')
+sys.path.append('../data/KAQP_atlantis')
+
+
+THIS_FOLDER = os.path.dirname(os.path.abspath(__file__))
+my_file = os.path.join(THIS_FOLDER, '../data/WTDL_pisces/WTDL_20120404v20001.nc')
+
+dataset = Dataset(my_file)
