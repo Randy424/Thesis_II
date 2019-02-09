a2b3df24e6976e629e7b6710d02b8e19b4e02cad
Randy <randybruno424@gmail.com>
2019-02-09 15:17:29 -0500

    modified load_dataset.py

--- sourcecode/load_dataset.py
+++ sourcecode/load_dataset.py
@@ -1,6 +1,10 @@
 import os
 import sys
 from netCDF4 import Dataset
+from sklearn import preprocessing
+from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import OneHotEncoder
+import pandas as pd
 
 sys.path.append('../data/WTDL_pisces')
 sys.path.append('../data/KAQP_atlantis')
@@ -34,7 +38,20 @@ time_of_day = dataset.variables['time_of_day'][:]
  'SPD', 'PL_WSPD', 'P', 'T', 'RH', 'TS', 'SSPS', 'date', 'time_of_day', 'flag', 'history']
 '''
 
+df = pd.DataFrame(data = flags, columns = ['time', 'lat', 'lon', 'PL_HD', 'PL_CRS', 'DIR', 'PL_WDIR', 'PL_SPD',
+ 'SPD', 'PL_WSPD', 'P', 'T', 'RH', 'TS', 'SSPS'])
 
 
-print (dataset.variables.keys())
-print (time_of_day)
+
+#Attempting to encode labels
+
+print(flags[0], "variables: ",dataset.variables)
+
+#label_encoder = preprocessing.LabelEncoder()
+#label_encoder.fit(flags)
+#print("label classes: ",list(label_encoder.classes_))
+
+#encoded_flags = label_encoder.transform(flags)
+#print("encoded flags: ",encoded_flags)
+#print (dataset.variables.keys())
+#print (time_of_day)
